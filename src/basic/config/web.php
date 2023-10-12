65d7fe4e76782569ea854247acce1c5dc2b47db6
guillermo santolaria <guillote.s07@gmail.com>
2023-10-11 23:05:54 -0300

    punto 8) del tp2 de pwa2023

--- src/basic/config/web.php
+++ src/basic/config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'dPqI_wyET3cbnipRzmPL86YxQB0MDJOx',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser'
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
